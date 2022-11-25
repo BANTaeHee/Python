1cb7ad21346c9863eef0c510377edbb25c0f4d65
BANTaeHee <ban7598@gmail.com>
2022-11-25 14:07:16 +0900

    22/11/25 Python 5회차 수업(클래스)

--- 9. 클래스/5. overriding_classvariable.py
+++ 9. 클래스/5. overriding_classvariable.py
@@ -7,6 +7,8 @@
             부모 클래스에 클래스변수 추가하기
 '''
 
+import random
+
 # 부모 클래스
 class Monster:
     max_num = 1000              #클래스변수
@@ -18,3 +20,35 @@ class Monster:
 
     def move(self):
         print(f"[{self.name}] 지상에서 이동하기")
+
+# 자식 클래스
+class Wolf(Monster):
+    pass
+
+class Shark(Monster):
+    def move(self):         # 메서드 오버라이딩
+        print(f"[{self.name}] 헤엄치기")
+
+class Dragon(Monster):
+    # 생성자 오버라이딩
+    def __init__(self, name, health, attack):
+        super().__init__(name, health, attack)
+        self.skills = ("불뿜기", "꼬리치기", "날개치기")
+    def move(self):
+        print(f"[{self.name}] 날기")
+
+    def skill(self):
+        print(f"[{self.name} 스킬사용 {self.skills[random.randint(0, 2)]}")
+
+wolf = Wolf("울프", 1500, 200 )
+wolf.move()
+print(wolf.max_num)
+
+shark = Shark("샤크", 3000, 400)
+shark.move()
+print(shark.max_num)
+
+dragon = Dragon("드래곤", 8000, 800)
+dragon.move()
+dragon.skill
+print(dragon.max_num)
